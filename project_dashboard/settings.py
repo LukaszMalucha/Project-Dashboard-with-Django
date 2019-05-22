40408e9c0c3f9ed09df735d3b9cd40647deef759
Lukasz Malucha <lucasmalucha@gmail.com>
2019-05-22 19:09:34 +0100

    travis

--- project_dashboard/settings.py
+++ project_dashboard/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = 'secret'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
